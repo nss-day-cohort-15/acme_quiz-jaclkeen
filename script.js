ef3bd993cac311d2d5ad764601cd542d3d06e1ff
Jacob Keen <jaclkeen@gmail.com>
2016-08-07 18:26:18 -0500

    category function changes

--- script.js
+++ script.js
@@ -24,43 +24,50 @@ Promise.all([
 function filter(categories, types, products){
   var $filtered = [];
   var $selection = $('#types').val();
+  var category;
+  var all_cat = []
   if($selection === 'fireworks'){
-    for(var key in products){
-      if(products[key].id === 0){
-        $filtered.push(products[key]);
+    for(var key in types){
+      if(types[key].id === 0){
+        category = categories[0].name
+        $filtered.push(types[key]);
       }
     }
   }
 
   else if($selection === 'demolition'){
-    for(var key in products){
-      if(products[key].id === 1){
-        $filtered.push(products[key]);
-    console.log("demoltion--------->", $filtered)
+    for(var key in types){
+      if(types[key].id === 1){
+        category = categories[1].name
+        $filtered.push(types[key]);
+    console.log("demolition--->", $filtered)
       }
     }
   }
 
   else if($selection === 'wmd'){
-    for(var key in products){
-      if(products[key].id === 2){
-        $filtered.push(products[key]);
+    for(var key in types){
+      if(types[key].id === 2){
+        category = categories[2].name
+        $filtered.push(types[key]);
         console.log($filtered)
       }
     }
   }
 
   else{
-    for(var key in products){
-      $filtered.push(products[key]);
+    for(var key in types){
+      $filtered.push(types[key]);
+      all_cat.push(types[key].id);
+      determineCat(all_cat, category)
     }
   }
 
-  console.log($filtered)
-  print($filtered);
+  console.log($filtered, category)
+  print($filtered, category);
 }
 
-function print(element){
+function print(element, categories){
   var $content = $('#json_area');
   $content.html("");
   element.forEach(function(item){
@@ -68,7 +75,7 @@ function print(element){
     $info.addClass('row');
     $info.html(`
       <td class='col-md-10'>${item.name}</td>
-      <td class='col-md-2'>${item.id}</td>
+      <td class='col-md-2'>${categories}</td>
     `)
     $content.append($info);
   })
@@ -77,7 +84,20 @@ function print(element){
 function filterForm(){
   var change = document.getElementById('types');
   change.addEventListener('change', function(){
-    console.log('working')
     filter(categories, types, products)
   })
+}
+
+function determineCat(array, variable){
+  for(var i = 0; i < array.length; i++){
+    if(array[i] === 0){
+      variable = 'Fireworks'
+    }
+    else if(array[i] === 1){
+      variable === "Demolition"
+    }
+    else{
+      variable === "Weapons of Mass Destruction"
+    }
+  }
 }
