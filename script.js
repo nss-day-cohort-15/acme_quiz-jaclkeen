8b59bfe616edb9a9f86a5889c4763fe2cb50b910
Jacob Keen <jaclkeen@gmail.com>
2016-08-07 17:15:09 -0500

    product filter works for everything but demolition

--- script.js
+++ script.js
@@ -1,63 +1,25 @@
-function loadCategories(){
-  return new Promise(function(resolve, reject){
-    $.ajax({
-      url: 'categories.json'
-    }).done(function(data){
-      resolve(data);
-    }).fail(function(xhr, status, error){
-      reject(error);
-    });
+var categories, types, products
+
+Promise.all([
+  $.getJSON('categories.json')
+    .then(function(r){
+      categories=r.categories
+    }),
+  $.getJSON('types.json')
+    .then(function(r){
+      types=r.types
+    }),
+  $.getJSON('products.json')
+  .then(function(r){
+    products=r.products[0]
+  }).then(function(){
+    filter(categories, types, products)
+    filterForm()
+    // console.log(products)
+    // console.log(types)
+    // console.log(categories)
   })
-};
-
-function loadTypes(){
-  return new Promise(function(resolve, reject){
-    $.ajax({
-      url: 'types.json'
-    }).done(function(data){
-      resolve(data);
-    }).fail(function(xhr, status, error){
-      reject(error);
-    })
-  })
-}
-
-function loadProducts(){
-  return new Promise(function(resolve, reject){
-    $.ajax({
-      url: 'products.json'
-    }).done(function(data){
-      resolve(data)
-    }).fail(function(xhr, status, error){
-      reject(error);
-    })
-  })
-}
-
-function load(){
-  var categories;
-  var types;
-  var products;
-
-  loadCategories()
-    .then(function(data1){
-      categories = data1;
-        console.log(categories)
-          return loadTypes(data1);
-    })
-    .then(function(data2){
-      types = data2;
-        console.log(types)
-          return loadProducts(data2);
-    })
-    .then(function(data3){
-      products = data3;
-        console.log(products);
-    })
-    .then(function(){
-      filter(categories, types, products);
-    });
-}
+])
 
 function filter(categories, types, products){
   var $filtered = [];
@@ -69,51 +31,53 @@ function filter(categories, types, products){
       }
     }
   }
+
   else if($selection === 'demolition'){
-    for(var key in products[0]){
+    for(var key in products){
       if(products[key].id === 1){
-        $filtered.push(products[0][key]);
+        $filtered.push(products[key]);
+        // console.log($filtered)
       }
     }
   }
+
   else if($selection === 'wmd'){
     for(var key in products){
       if(products[key].id === 2){
         $filtered.push(products[key]);
-        console.log(filtered)
+        console.log($filtered)
       }
     }
   }
+
   else{
     for(var key in products){
-      $filtered += products[key];
+      $filtered.push(products[key]);
     }
   }
+
   console.log($filtered)
   print($filtered);
 }
 
 function print(element){
   var $content = $('#json_area');
-  var $info = $('<tr></tr>');
+  $content.html("");
   element.forEach(function(item){
+    var $info = $('<tr></tr>');
     $info.addClass('row');
     $info.html(`
-      <td class = 'col-md-10'>${item.name}</td>
-      <td class = 'col-md-2'>${item.id}</td>
+      <td class='col-md-10'>${item.name}</td>
+      <td class='col-md-2'>${item.id}</td>
     `)
     $content.append($info);
   })
 }
 
-function execute(){
-  load();
-  var $selection = $('#types');
-  $selection.on('change', function(){
-    load();
-    console.log('working');
-    filter()
-  });
+function filterForm(){
+  var change = document.getElementById('types');
+  change.addEventListener('change', function(){
+    console.log('working')
+    filter(categories, types, products)
+  })
 }
-
-execute();
